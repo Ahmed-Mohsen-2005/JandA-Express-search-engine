255b9d1e0aad5d4bb90a4b1ac5f922c6c1352c98
Jana Ahmed <jana.ah.alnajjar@gmail.com>
2025-05-01 12:25:07 +0300

    main file

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, render_template
+import sqlite3
+import pandas as pd
+from sqlalchemy import create_engine
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except:
+        print("db not exist")
+    
+    return conn
+
+
+df = pd.read_csv("our_corpus_preprocess.csv")
+# df2= pd.read_csv("our_corpus_without_preprocess.csv")
+# print(df)
+con = create_connection("corpus_processed.db")
+
+df.to_sql('our_corpus_preprocess', con, if_exists='replace')
+con.close();
+
+db_url = 'sqlite:///corpus_processed.db'
+engine = create_engine(db_url, echo= True)
+df_2 = pd.read_sql('select * from our_corpus_preprocess', engine)
+print(df_2)
+
+app = Flask(__name__)
